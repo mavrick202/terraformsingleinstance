aaac44bc2d89d4f2600176cf2981ca22f375f805
Sreeharsha <P10312228@capita.co.uk>
2018-04-06 14:27:35 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name = "${var.vpc_name}"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags {
+        Name = "${var.IGW_name}"
+    }
+}
+
+resource "aws_subnet" "subnet1-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet1_cidr}"
+    availability_zone = "us-east-1a"
+
+    tags {
+        Name = "${var.public_subnet1_name}"
+    }
+}
+
+resource "aws_subnet" "subnet2-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet2_cidr}"
+    availability_zone = "us-east-1b"
+
+    tags {
+        Name = "${var.public_subnet2_name}"
+    }
+}
+
+resource "aws_subnet" "subnet3-public" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.public_subnet3_cidr}"
+    availability_zone = "us-east-1c"
+
+    tags {
+        Name = "${var.public_subnet3_name}"
+    }
+	
+}
+
+
+resource "aws_route_table" "terraform-public" {
+    vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+
+    tags {
+        Name = "${var.Main_Routing_Table}"
+    }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    route_table_id = "${aws_route_table.terraform-public.id}"
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+ }	
+
+resource "aws_instance" "web-1" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+	count = 3
+    #availability_zone = "${element(var.azs, count.index)}"
+    instance_type = "t2.nano"
+    key_name = "${var.key_name}"
+	subnet_id = "${element(data.aws_subnet_ids.private.ids, count.index)}"
+	vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+	
+    tags {
+        Name = "Server-${count.index}"
+    }
+}
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+    }
+}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+variable "azs" {
+  description = "Run the EC2 Instances in these Availability Zones"
+  type = "list"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}

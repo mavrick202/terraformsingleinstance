62962a4e48a068995f3ded07074f3f29e0400f75
Mavrick-DevOps <mavrick202@gmail.com>
2019-03-17 07:38:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,15 +92,15 @@ resource "aws_security_group" "allow_all" {
 data "aws_ami" "my_ami" {
       most_recent      = true
       #name_regex       = "^mavrick"
-      owners           = ["0534900xxxxx"]
+      owners           = ["053490018989"]
 }
 
 
 resource "aws_instance" "web-1" {
-    ami = "${lookup(var.amis, var.aws_region)}"
+    ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.nano"
-    key_name = "${var.key_name}"
+    key_name = "LaptopKey"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	

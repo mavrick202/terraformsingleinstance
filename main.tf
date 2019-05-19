56d768e97adc1d58f153891b72dd1cbb2157673c
mavrick202 <mavrick202@gmail.com>
2019-05-19 07:20:33 +0530

    Without EC2 & Outputfile

--- main.tf
+++ main.tf
@@ -88,31 +88,3 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks     = ["0.0.0.0/0"]
     }
 }
-
-data "aws_ami" "my_ami" {
-      most_recent      = true
-      #name_regex       = "^mavrick"
-      owners           = ["053490018989"]
-}
-
-
-resource "aws_instance" "web-1" {
-    ami = "${data.aws_ami.my_ami.id}"
-    availability_zone = "us-east-1a"
-    instance_type = "t2.nano"
-    key_name = "LaptopKey"
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true	
-    tags {
-        Name = "Server-${count.index}"
-        Env = "Prod"
-        Owner = "Sree"
-    }
-}
-
-output "ami_id" {
-  value = "${data.aws_ami.my_ami.id}"
-}
-
-

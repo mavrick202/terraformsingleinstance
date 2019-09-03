7d76c09e6678977112860ad41aa1472d9f98e861
mavrick202 <mavrick202@gmail.com>
2019-09-03 07:43:50 +0530

    New Branch for S3 Backend

--- main.tf
+++ main.tf
@@ -89,29 +89,10 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-#data "aws_ami" "my_ami" {
-#      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["444984551434"]
-#}
-
-
-resource "aws_instance" "web-1" {
-    #ami = "${data.aws_ami.my_ami.id}"
-    ami = "ami-0d857ff0f5fc4e03b"
-    availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
-    key_name = "LaptopKey"
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true	
-    tags = {
-        Name = "Server-1"
-        Env = "Prod"
-        Owner = "Sree"
-    }
+terraform {
+  backend "s3" {
+    bucket = "sreeterraformbucket"
+    key    = "myterraform.tfstate"
+    region = "us-east-1"
+  }
 }
-
-#output "ami_id" {
-#  value = "${data.aws_ami.my_ami.id}"
-#}

1f77125643fea8a16b1dcaf6bbb00ebea9ddeadb
Mavrick-DevOps <mavrick202@gmail.com>
2020-02-02 01:54:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,21 +97,21 @@ data "aws_ami" "my_ami" {
 }
 
 
-resource "aws_instance" "web-1" {
-    ami = "${data.aws_ami.my_ami.id}"
-    #ami = "ami-0d857ff0f5fc4e03b"
-    availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
-    key_name = "LaptopKey"
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    associate_public_ip_address = true	
-    tags = {
-        Name = "Server-1"
-        Env = "Prod"
-        Owner = "Sree"
-    }
-}
+# resource "aws_instance" "web-1" {
+#     ami = "${data.aws_ami.my_ami.id}"
+#     #ami = "ami-0d857ff0f5fc4e03b"
+#     availability_zone = "us-east-1a"
+#     instance_type = "t2.micro"
+#     key_name = "LaptopKey"
+#     subnet_id = "${aws_subnet.subnet1-public.id}"
+#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+#     associate_public_ip_address = true	
+#     tags = {
+#         Name = "Server-1"
+#         Env = "Prod"
+#         Owner = "Sree"
+#     }
+# }
 
 #output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"

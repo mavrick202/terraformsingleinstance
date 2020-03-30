1ecbf43a3cb13ed6e80547287b8491f4be027790
Mavrick-DevOps <mavrick202@gmail.com>
2020-03-30 02:49:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
     #access_key = "${var.aws_access_key}"
     #secret_key = "${var.aws_secret_key}"
-    #region = "${var.aws_region}"
+    region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "default" {

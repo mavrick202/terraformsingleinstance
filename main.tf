f7afda818d6aa68829322418f461177326a01678
kushwanth19 <64457884+kushwanth19@users.noreply.github.com>
2020-05-13 11:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_security_group" "allow_all" {
 data "aws_ami" "my_ami" {
      most_recent      = true
      #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
+     owners           = ["284837235494"]
 }
 
 

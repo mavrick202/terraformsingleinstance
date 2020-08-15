5e5933678b88acbb4461527644bbf06ef2316a47
Mavrick-DevOps <mavrick202@gmail.com>
2020-08-15 02:00:49 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -99,8 +99,8 @@ resource "aws_security_group" "allow_all" {
 
 
 resource "aws_instance" "web-1" {
-    ami = var.imagename
-    #ami = "ami-0d857ff0f5fc4e03b"
+    #ami = var.imagename
+    ami = "ami-0d857ff0f5fc4e03b"
     availability_zone = "us-east-1a"
     instance_type = "t2.nano"
     key_name = "LaptopKey"

4bb9c81117b602c254b268900db1a999d4042591
Mavrick-DevOps <mavrick202@gmail.com>
2020-08-15 03:49:19 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -124,6 +124,9 @@ resource "aws_instance" "web-1" {
 # echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
 # echo "Running Packer Now...!!"
 # packer build -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
+#packer validate --var-file creds.json packer.json
+#packer build --var-file creds.json packer.json
+#packer.exe build --var-file creds.json -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
 # echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
 # echo "Running Terraform Now...!!"
 # terraform init

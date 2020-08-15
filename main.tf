7de653219841bf9fb18442b20dfba3ce19ba2cab
Mavrick-DevOps <mavrick202@gmail.com>
2020-08-15 01:59:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_instance" "web-1" {
     ami = var.imagename
     #ami = "ami-0d857ff0f5fc4e03b"
     availability_zone = "us-east-1a"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     key_name = "LaptopKey"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]

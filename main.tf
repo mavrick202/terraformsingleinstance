7aff0c9b73dd2ecb4c10b3aae84cd53319fad84b
Mavrick-DevOps <mavrick202@gmail.com>
2020-10-24 03:40:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "default" {
     enable_dns_hostnames = true
     tags = {
         Name = "${var.vpc_name}"
-	Owner = "Sreehasha"
+	Owner = "Sreeharsha Veerapalli"
 	environment = "${var.environment}"
     }
 }

92fee4c5bfbe8d7b40231ee74e7b97acc8be12d5
Mavrick-DevOps <mavrick202@gmail.com>
2021-06-07 07:19:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,10 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "<= 0.14" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 3.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
+  backend "s3" {
+    bucket = "sreeterraformbucket"
+    key    = "jenkins.tfstate"
+    region = "us-east-1"
   }
 }
 

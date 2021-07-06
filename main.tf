4da36e3db0f2a606640c58db5563783e0df604ac
Mavrick-DevOps <mavrick202@gmail.com>
2021-07-06 07:42:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ provider "aws" {
 #   }
 # }
 
+terraform {
+  backend "s3" {
+    bucket = "k8sb06cluster"
+    key    = "dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true

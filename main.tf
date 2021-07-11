09ea59c5086535759123593dac4f44acded16644
Mavrick-DevOps <mavrick202@gmail.com>
2021-07-11 07:40:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "sreeterraformbucket"
+    key    = "devopsb18jenkins.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true

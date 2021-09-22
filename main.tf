4b9723649f7328a5706dbc6788d5f1598ec482d2
Mavrick-DevOps <mavrick202@gmail.com>
2021-09-22 08:22:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "sreeterraformbucket"
+    key    = "devopsb19jenkins.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true

8b0633ffbdb7a84f21ea41e597f28b26cb02c86e
manig139 <manig.devops@gmail.com>
2022-01-23 16:45:16 -0500

    base commit

--- main.tf
+++ main.tf
@@ -95,4 +95,31 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks     = ["0.0.0.0/0"]
     }
 }
+ data "aws_ami" "my_ami" {
+      most_recent      = true
+      #name_regex       = "^mavrick"
+      owners           = ["256883250560"]
+ }
 
+
+ resource "aws_instance" "web-1" {
+     #ami = var.imagename
+     #ami = "ami-0d857ff0f5fc4e03b"
+     ami = "${data.aws_ami.my_ami.id}"
+     availability_zone = "us-east-1a"
+     instance_type = "t2.micro"
+     key_name = "instkey.pem"
+     subnet_id = "${aws_subnet.subnet1-public.id}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-1"
+         Env = "Prod"
+         Owner = "mike"
+ 	CostCenter = "9400"
+     }
+ }
+
+output "ami_id" {
+  value = "${data.aws_ami.my_ami.id}"
+}

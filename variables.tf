853a61c0d760838bb5dfc04fc547096ca7dc3fd5
Mavrick-DevOps <mavrick202@gmail.com>
2023-04-10 08:16:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"

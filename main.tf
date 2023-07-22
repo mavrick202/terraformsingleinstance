d08a0625756f7c817133c845283a5fbc31b730b2
Mavrick-DevOps <mavrick202@gmail.com>
2023-07-22 07:08:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,23 +109,24 @@ resource "aws_security_group" "allow_all" {
 # }
 
 
-# resource "aws_instance" "web-1" {
-#     ami = var.imagename
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     #ami = "${data.aws_ami.my_ami.id}"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-# }
+resource "aws_instance" "web-1" {
+     ami = var.imagename
+     #ami = "ami-0d857ff0f5fc4e03b"
+     #ami = "${data.aws_ami.my_ami.id}"
+     availability_zone = "us-east-1a"
+      associate_public_ip_address = true
+     instance_type = "t2.micro"
+     key_name = "LaptopKey"
+     subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-1"
+         Env = "Prod"
+         Owner = "Sree"
+ 	CostCenter = "ABCD"
+     }
+ }
 
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"

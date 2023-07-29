fa9f079c633ad3e25902ac07fa7106cfe5afe56f
Mavrick-DevOps <mavrick202@gmail.com>
2023-07-29 06:12:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,7 +119,6 @@ resource "aws_instance" "web-1" {
      key_name = "LaptopKey"
      subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-     associate_public_ip_address = true	
      tags = {
          Name = "Server-1"
          Env = "Prod"

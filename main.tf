288a0fb9991b21771a3153a28586c34b4d792a46
Mavrick-DevOps <mavrick202@gmail.com>
2023-07-29 06:11:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "<= 1.3.14" #Forcing which version of Terraform needs to be used
+  required_version = "<= 1.5.14" #Forcing which version of Terraform needs to be used
   required_providers {
     aws = {
       version = "<= 5.0.0" #Forcing which version of plugin needs to be used.

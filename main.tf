52e346b36180cdee70115b12256db966832ee3c0
Mavrick-DevOps <mavrick202@gmail.com>
2023-07-29 06:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,17 +102,15 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-# data "aws_ami" "my_ami" {
-#      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
-# }
+data "aws_ami" "my_ami" {
+      most_recent      = true
+      #name_regex       = "^mavrick"
+      owners           = ["721834156908"]
+ }
 
 
 resource "aws_instance" "web-1" {
-     ami = var.imagename
-     #ami = "ami-0d857ff0f5fc4e03b"
-     #ami = "${data.aws_ami.my_ami.id}"
+     ami = "${data.aws_ami.my_ami.id}"
      availability_zone = "us-east-1a"
       associate_public_ip_address = true
      instance_type = "t2.micro"

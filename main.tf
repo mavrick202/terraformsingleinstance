cb3de2f1d54f9d04444bf474e59d725587cd5a91
Mavrick-DevOps <mavrick202@gmail.com>
2023-07-29 06:16:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    #access_key = "${var.aws_access_key}"
+    #secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 

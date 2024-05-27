4b34f8024424414208cf67be3b95a8e214d7a4c2
Mavrick-DevOps <mavrick202@gmail.com>
2024-05-27 07:37:24 +0530

    Updated For Jenkins Params and Multi AWS Account.

---
+++ 2.provider-prod.tf
@@ -0,0 +1,20 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+    region = var.aws_region
+    profile = var.aws_profile
+}
+
+terraform {
+  required_version = "<= 2.3.14"
+  required_providers {
+    aws = {
+      version = "<= 6.0.0"
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+      key = "prod_terraform.tfstate"
+      bucket = "sreeterraformbucket"
+      region = "us-east-1"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-#This Terraform Code Deploys Basic VPC Infra.
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
-
-terraform {
-  required_version = "<= 1.3.14" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 5.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
@@ -102,45 +85,19 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-# data "aws_ami" "my_ami" {
-#      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
-# }
 
-
-# resource "aws_instance" "web-1" {
-#     ami = var.imagename
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     #ami = "${data.aws_ami.my_ami.id}"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-# }
-
-##output "ami_id" {
-#  value = "${data.aws_ami.my_ami.id}"
-#}
-#!/bin/bash
-# echo "Listing the files in the repo."
-# ls -al
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Packer Now...!!"
-# packer build -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-#packer validate --var-file creds.json packer.json
-#packer build --var-file creds.json packer.json
-#packer.exe build --var-file creds.json -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Terraform Now...!!"
-# terraform init
-# terraform apply --var-file terraform.tfvars -var="aws_access_key=AAAAAAAAAAAAAAAAAA" -var="aws_secret_key=BBBBBBBBBBBBB" --auto-approve
-#https://discuss.devopscube.com/t/how-to-get-the-ami-id-after-a-packer-build/36
+resource "aws_instance" "web-1" {
+    ami = "ami-04b70fa74e45c3917"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    key_name = "LaptopKey"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+    associate_public_ip_address = true	
+    tags = {
+        Name = "Server-1"
+        Env = "Prod"
+        Owner = "Sree"
+	     CostCenter = "ABCD"
+    }
+}

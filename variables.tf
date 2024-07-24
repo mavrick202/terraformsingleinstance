7728d34799bccee8e4df1a85b141025158076c22
Mavrick-DevOps <mavrick202@gmail.com>
2024-07-24 09:15:00 +0530

    Updated Code For AWS B61 EFS Jenkins

--- variables.tf
+++ variables.tf
@@ -1,15 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
-variable "amis" {
-    description = "AMIs by region"
-    default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
-    }
-}
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
@@ -17,22 +8,9 @@ variable "key_name" {}
 variable "public_subnet1_cidr" {}
 variable "public_subnet2_cidr" {}
 variable "public_subnet3_cidr" {}
-variable "private_subnet_cidr" {}
 variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
-variable "private_subnet_name" {}
 variable Main_Routing_Table {}
-variable "azs" {
-  description = "Run the EC2 Instances in these Availability Zones"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
 variable "environment" { default = "dev" }
-variable "instance_type" {
-  default = {
-    dev = "t2.nano"
-    test = "t2.micro"
-    prod = "t2.medium"
-    }
-}
 
